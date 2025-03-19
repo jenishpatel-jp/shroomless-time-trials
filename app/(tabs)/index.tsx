61ef4b9023214ff2e59412d741fbb870412101fc
Jenish <jpat534@gmail.com>
2025-03-19 20:08:45 +1100

    Added code to test how images would work

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,18 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { Image } from 'react-native';
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Old maps</Text> 
+      <Image 
+        source={require('@/assets/images/Old Maps/Mushroom Cup/Mario Kart Stadium.png')}       
+        style={{ width: 200, height: 200 }}
+        resizeMode="contain"
+      />
+    
     </View>
   );
 }
