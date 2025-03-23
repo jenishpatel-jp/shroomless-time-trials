420afdf871ed2411673f1d4e712d011d1dfa0353
Jenish <jpat534@gmail.com>
2025-03-23 19:21:55 +1100

    Added an AddTime component

---
+++ components/timeTrialBoard/AddTime.tsx
@@ -0,0 +1,19 @@
+import { TextInput, View } from 'react-native'
+
+const AddTime = () => {
+
+    return (
+        <View> 
+            <TextInput 
+                placeholder="Add Time"
+                placeholderTextColor={'white'}
+                // onChangeText={setTime}
+                // value={time}
+                // style={styles.input
+            />
+            
+        </View>
+    )
+}
+
+export default AddTime
