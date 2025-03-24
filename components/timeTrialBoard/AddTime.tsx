e191a7de89de6175aa5c3df31186461b513c9047
Jenish <jpat534@gmail.com>
2025-03-24 18:36:13 +1100

    Created the AddTime component and added the Add Button to it

--- components/timeTrialBoard/AddTime.tsx
+++ components/timeTrialBoard/AddTime.tsx
@@ -1,16 +1,29 @@
-import { TextInput, View } from 'react-native'
+import { TextInput, View, StyleSheet } from 'react-native'
+import AddButton from '../buttons/AddButton'
 
 const AddTime = () => {
 
     return (
         <View> 
             <TextInput 
-                placeholder="Add Time"
+                placeholder="0:00.000"
+                style={styles.container}
                 placeholderTextColor={'white'}
                 onChangeText={(text) => console.log(text)}
             />
+            <AddButton />
         </View>
     )
 }
 
 export default AddTime
+
+const styles = StyleSheet.create({
+    container: {  
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'black',
+    }
+
+})
