baf925e750825b4ae21f2bef11ddcf0a2f7ffb82
Jenish <jpat534@gmail.com>
2025-03-24 22:00:07 +1100

    Added code to insert a time trial as well as update the packages

--- lib/db.ts
+++ lib/db.ts
@@ -11,3 +11,40 @@ export const openDatabase = async () => {
         throw error;
     }
 };
+
+// Set up the database schema
+
+export const setupSTTDatabase = async () => {
+    try {
+        const db = await openDatabase();
+        await db.execAsync('PRAGMA jounal_mode=WAL;');
+
+        await db.execAsync(`
+            CREATE TABLE IF NOT EXISTS stt (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                time TEXT NOT NULL
+            )
+            `);
+        console.log('Shroomless Time Trial table successfully created');
+    } catch (error) {
+        console.error('Error setting up database:', error);
+        throw error
+    }
+};
+
+// Insert a new time trial record
+
+export const insertSTT = async (name: string, time: string, callback?: () => void) => {
+    try {
+        const db = await openDatabase();
+        await db.runAsync(
+            'INSERT INTO stt (name, time) VALUES (?, ?)',
+            [name, time]
+        );
+        console.log('New record successfully inserted');
+    } catch (error) {
+        console.error('Error inserting record:', error);
+        throw error;
+    }
+};
