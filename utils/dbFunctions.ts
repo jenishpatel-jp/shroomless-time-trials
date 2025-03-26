42e48475451eb7f71544b0f5bec7145089bb22f6
Jenish <jpat534@gmail.com>
2025-03-26 20:09:35 +1100

    Added an addTime function

--- utils/dbFunctions.ts
+++ utils/dbFunctions.ts
@@ -2,4 +2,17 @@ import { useSQLiteContext } from "expo-sqlite";
 
 export const useDatabase = () => {
     const db = useSQLiteContext();
+
+    const addTime = async (map:string, time: string, callback?: ()=> void) => {
+        try {
+            const result = await db.runAsync(
+                'INSERT INTO time (time) VALUES (?);', [time]
+            );
+            console.log(`Time added successfully with ID: ${result.lastInsertRowId}`);
+            if (callback) callback();
+        } catch (error){
+            console.error('Error adding time:', error);
+        }
+    };
+
 }
