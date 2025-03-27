c02b7e26328507ce61ec891670cfe670c7fed161
Jenish <jpat534@gmail.com>
2025-03-27 18:03:50 +1100

    Added a getTimes database function

--- utils/dbFunctions.ts
+++ utils/dbFunctions.ts
@@ -15,16 +15,16 @@ export const useDatabase = () => {
         }
     };
 
-    const deleteTime = async (id:number, callback?: ()=> void) => {
+    const deleteTime = async (time:string, callback?: ()=> void) => {
         try {
             const result = await db.runAsync(
-                `DELETE FROM sst WHERE id = ?`, [id]
+                `DELETE FROM sst WHERE time = ?`, [time]
             );
 
             if (result.changes > 0){
-                console.log(`Time wtih ID ${id} was successfully deleted`)
+                console.log(`Time wtih ID ${time} was successfully deleted`)
             } else {
-                console.log(`No time with ${id} to delete.`)
+                console.log(`No time with ${time} to delete.`)
             }
 
             if (callback) callback();
@@ -32,8 +32,42 @@ export const useDatabase = () => {
         } catch(error){
             console.error('Error deleting the time', error);
         }
-    }
+    };
 
-    
+    type TimeRow = {
+        id: number;
+        map: string;
+        time: string;
+    } 
+
+    const getTimes = async () => {
+        try {
+            const allRows: TimeRow[] = await db.getAllAsync('SELECT * FROM sst');
+            console.log('All times:', allRows);
+
+            const result: Record<string, [string][]> = {}
+
+            allRows.forEach((row:any) => {
+                
+                const map = row.map;
+                const time = row.time;
+
+                if (!result[map]){
+                    result[map] = [];
+                }
+
+                if (time !== null){
+                    result[map].push([time]);
+                }
+            });
+
+            return result;
+            // Should return in the format { map: [time, time], map: [time, time, time], map: [time]}
+
+        } catch(error){
+            console.error('Error getting times for each map', error);
+            return {};
+        }
+    }
 
 }
