a8fe341b6417fc3cb88ad64d2aaab07a432fe553
Jenish <jpat534@gmail.com>
2025-04-29 22:39:28 +1000

    Added a test function but still need to make the test pass.
    getByText might not be used correctly

--- __tests__/TabOneScreen-test.tsx
+++ __tests__/TabOneScreen-test.tsx
@@ -1,11 +1,18 @@
 import { render } from '@testing-library/react-native';
 import TabOneScreen from '../app/(tabs)/index';
 import { oldMaps } from '@/utils/oldMapUtils';
-import { Text } from 'react-native';
 
 const sum = (a: number, b: number) => a + b;
 
+
 describe('TabOneScreen', () => {
+
+    test('renders items correctly', () => {
+        const { getByText } = render(<TabOneScreen />);
+        expect(getByText('Mario Kart Stadium')).toBeVisible();
+    });
+    
+
     test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
     });
