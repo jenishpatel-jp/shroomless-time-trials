9f9b91a5a29f4445b5012eed55f7e11811a25698
Jenish <jpat534@gmail.com>
2025-05-01 17:58:52 +1000

    Added boilerplate to start testing if the MapTime component renders correctly

---
+++ __tests__/mapTime-test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react-native";
+import MapTime from "../components/timeTrialBoard/MapTime";
+
+describe("MapTime", () => {
+    test("renders correctly with given time", () => {
+
+    });
+});
