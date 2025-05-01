0b14ad922c4ac6261af4d8b6bedc7a7fb02050d5
Jenish <jpat534@gmail.com>
2025-05-01 22:14:03 +1000

    updated the mapTime test.

--- __tests__/mapTime-test.tsx
+++ __tests__/mapTime-test.tsx
@@ -3,6 +3,11 @@ import MapTime from "../components/timeTrialBoard/MapTime";
 
 describe("MapTime", () => {
     test("renders correctly with given time", () => {
+        const time = "00:01:23";
+        render(<MapTime time={time} />);
+
+        // use screen.getByText to find the text in the rendered component
+        const timeText = screen.getByText(time);
 
     });
 });
