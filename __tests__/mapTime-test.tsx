2f8d9a818ae1a815b8139a6e122c0785589b92b9
Jenish <jpat534@gmail.com>
2025-05-03 22:11:59 +1000

    Added boilerplate code for future tests

--- __tests__/mapTime-test.tsx
+++ __tests__/mapTime-test.tsx
@@ -9,6 +9,17 @@ describe("MapTime", () => {
         // use screen.getByText to find the text in the rendered component
         const timeText = screen.getByRole("text", { name: time });
         expect(timeText).toBeTruthy(); // Check if the text is present
+        expect(timeText.props.children).toBe(time); // Check if the text matches the expected time
 
     });
+
+
+    //testing if the element is on screen
+    test("element to be on screen", () => {
+        render(<MapTime time="00:01:23" />);
+    })
+
+    //testing if the text is on screen
+
+    //testing if the component has a prop
 });
