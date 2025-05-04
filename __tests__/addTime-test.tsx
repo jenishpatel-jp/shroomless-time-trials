2746b4d1892ba9008a29ae0e379625bdafe6d3da
Jenish <jpat534@gmail.com>
2025-05-04 20:30:54 +1000

    added mockProps and beforeEach to the addTime test

--- __tests__/addTime-test.tsx
+++ __tests__/addTime-test.tsx
@@ -2,5 +2,16 @@ import { render, screen } from '@testing-library/react-native';
 import AddTime from '@/components/timeTrialBoard/AddTime';
 
 describe('AddTime', () => {
+
+    const mockProps = {
+        map: 'testMap',
+        handleAddTime: jest.fn(),
+        setTrigger: jest.fn(),
+        addTime: jest.fn(),
+    };
+
+    beforeEach(() => {
+        render(<AddTime {...mockProps} />);
+    });
     
 });
