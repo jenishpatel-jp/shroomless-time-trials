f8397884dac54a502a3fe18833c13f390ddc9e85
Jenish <jpat534@gmail.com>
2025-05-04 15:39:38 +1000

    Updated the mapTime test to go through an array of times

--- __tests__/mapTime-test.tsx
+++ __tests__/mapTime-test.tsx
@@ -2,24 +2,15 @@ import { render, screen } from "@testing-library/react-native";
 import MapTime from "../components/timeTrialBoard/MapTime";
 
 describe("MapTime", () => {
-    test("renders correctly with given time", () => {
-        const time = "00:01:23";
-        render(<MapTime time={time} />);
-
-        // use screen.getByText to find the text in the rendered component
-        const timeText = screen.getByRole("text", { name: time });
-        expect(timeText).toBeTruthy(); // Check if the text is present
-        expect(timeText.props.children).toBe(time); // Check if the text matches the expected time
-
-    });
-
 
     //testing if the element is on screen
-    test("element to be on screen", () => {
-        render(<MapTime time="00:01:23" />);
-    })
+    test("renders a time", () => {
+        const times = ["00:00.000", "01:00:23", "3:43.123"];
 
-    //testing if the text is on screen
-
-    //testing if the component has a prop
+        times.forEach((time) => {
+            const { unmount } = render(<MapTime time={time} />);
+            expect(screen.getByText(time)).toBeOnTheScreen();
+            unmount();
+        });
+    });
 });
