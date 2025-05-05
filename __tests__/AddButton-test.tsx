2e14b44bdf6c2464ef932c10667b7bf5d01837b7
Jenish <jpat534@gmail.com>
2025-05-05 21:47:29 +1000

    Added a test to check if the button text has the correct style

--- __tests__/AddButton-test.tsx
+++ __tests__/AddButton-test.tsx
@@ -12,16 +12,25 @@ describe('AddButton', () => {
         addTime: jest.fn()
     };
 
+    const user = userEvent.setup();
+
     beforeEach(() => {
         render(<AddButton {...mockProps} />);
     });
 
-    // Test to check if the button is rendered correctly
     test('It renders the button with correct text', () => {
         const button = screen.getByRole('button', { name: /Add/i });
         expect(button).toBeOnTheScreen();
     });
 
-    // Test the button press functionality calls the correct function with the right arguments
+    test('Add text has correct style', () => {
+        const buttonText = screen.getByText('Add');
+        expect(buttonText).toHaveStyle({
+            color: '#2CBDFE',
+            fontSize: 20,
+            textAlign: 'center',
+        });
+
+    });
 
 });
