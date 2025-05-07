a1402c7ad11b21cfd6be3dc3211b385c7aa735d6
Jenish <jpat534@gmail.com>
2025-05-07 20:43:04 +1000

    added renderRouter test

--- __tests__/MapContainer-test.tsx
+++ __tests__/MapContainer-test.tsx
@@ -1,6 +1,7 @@
-import { render, screen, userEvent } from "@testing-library/react-native";
+import { screen } from "@testing-library/react-native";
 import MapContainer from "@/components/mapContainer/mapContainer";
 import { renderRouter } from "expo-router/testing-library";
+import { View } from "react-native";
 
 describe("MapContainer", () => {
 
@@ -9,14 +10,22 @@ describe("MapContainer", () => {
         name: "testMap",
     };
 
-    beforeEach(() => {
-        render(<MapContainer {...mockProps} />);
-    });
-
     test("renders the map image", () => {
+        renderRouter(
+            {
+                index: () => <MapContainer {...mockProps} />,
+                "map/[mapName]": () => <View testID="map-image"/>,
+ 
+            },
+            {
+                initialUrl: "/",
+            }
+        )
+
         const image = screen.getByTestId("map-image");
         expect(image).toBeOnTheScreen();
     });
 
+   
 
 });
