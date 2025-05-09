17b52cee83fb9ed1aa1f299f41a2ce4dcfa75a2c
Jenish <jpat534@gmail.com>
2025-05-09 18:02:39 +1000

    Added test for the TimeTrial Board

--- components/timeTrialBoard/__tests__/TimeTrialBoard-test.tsx
+++ components/timeTrialBoard/__tests__/TimeTrialBoard-test.tsx
@@ -1,14 +1,27 @@
 import { render, screen } from "@testing-library/react-native";
-import MapTimeContainer from "../MapTimeContainer";
+import TimeTrialBoard from "../TimeTrialBoard";
 
-describe("MapTimeContainer", () => {
+describe("TimeTrialBoard", () => {
 
     const mockProps = {
-        time: "1:23.456",
+        map: "testMap",
+        mapAndTime: {
+            testMap: ["00:00.000", "01:00.123", "02:30.456"],
+        },
+        handleAddTime: jest.fn(),
         handleDeleteTime: jest.fn(),
         setTrigger: jest.fn(),
+        addTime: jest.fn(),
         deleteTime: jest.fn(),
-    };    
+    };
 
+    beforeEach(() => {
+        render(<TimeTrialBoard {...mockProps} />);
+    });
+
+    test("renders TimeTrialBoard", () => {
+        expect(screen.getByText("testMap")).toBeOnTheScreen();
+        expect(screen.getByText("00:00.000")).toBeOnTheScreen();
+    });
 
 });
