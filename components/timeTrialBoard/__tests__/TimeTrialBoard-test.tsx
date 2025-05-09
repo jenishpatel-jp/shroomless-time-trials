e0604188f5041ae46110ebe4ee084af9ea937825
Jenish <jpat534@gmail.com>
2025-05-09 17:58:44 +1000

    Added a test for the TImeTrialBoad and added boiler plate code

---
+++ components/timeTrialBoard/__tests__/TimeTrialBoard-test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react-native";
+import MapTimeContainer from "../MapTimeContainer";
+
+describe("MapTimeContainer", () => {
+
+    const mockProps = {
+        time: "1:23.456",
+        handleDeleteTime: jest.fn(),
+        setTrigger: jest.fn(),
+        deleteTime: jest.fn(),
+    };    
+
+
+});
