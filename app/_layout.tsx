c7ea2160e5f1677c48b82c514b0021ec755f553a
Jenish <jpat534@gmail.com>
2025-05-13 18:06:23 +1000

    Removed a function for /modal as it is no longer being used

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,10 +14,6 @@ export {
   ErrorBoundary,
 } from 'expo-router';
 
-export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
-};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -47,7 +43,6 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
-
 
   return (
     <SQLiteProvider databaseName='stt' onInit={setupDatabase} >
